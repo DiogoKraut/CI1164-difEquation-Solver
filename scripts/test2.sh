d901650b6566a0bef73fdf69f20001b1b126ccd2
DiogoKraut <diogo.kraut@hotmail.com>
2019-11-14 17:57:07 -0300

    Removed scripts

--- scripts/test2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm scripts/L3.dat
-for i in {32,50}; do
-    sudo likwid-perfctr -C 2 -g L3 -m ./pdeSolver -nx $i -ny $i > out2.dat
-    cat out2.dat | grep "L3 bandwidth" | awk '{print $6}' >> scripts/L3.dat
-done
--- scripts/test3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm scripts/flops.dat
-for i in {32,200}; do
-    sudo likwid-perfctr -C 2 -g FLOPS_DP -m ./pdeSolver -nx $i -ny $i > out2.dat
-    cat out2.dat | grep "DP MFLOPS" | awk '{print $6}' >> scripts/flops.dat
-    cat out2.dat | grep "AVX DP"    | awk '{print $6}' >> scripts/avx.dat
-
-done
